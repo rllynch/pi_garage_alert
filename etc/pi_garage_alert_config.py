d3e9e284aa21f18ae8cda4197e0ee4d47a2d7446
tom stear <tuhsteh@users.noreply.github.com>
2016-08-18 23:56:03 -0600

    Update pi_garage_alert_config.py
    adding email X-Priority header config value

--- etc/pi_garage_alert_config.py
+++ etc/pi_garage_alert_config.py
@@ -53,6 +53,8 @@ SMTP_PORT = 25
 SMTP_USER = ''
 SMTP_PASS = ''
 EMAIL_FROM = 'Garage Door <user@example.com>'
+EMAIL_PRIORITY = '1'
+# 1 High, 3 Normal, 5 Low
 
 ##############################################################################
 # Twitter settings
